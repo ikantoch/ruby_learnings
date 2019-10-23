d8bf0bdee30a0d5d2ccdaa4d0d90ffb87a4347e4
ikantoch <ikantoch@zendesk.com>
2019-10-23 16:26:09 +0200

    refactor puzzle_4 to method

--- puzzles/puzzles.rb
+++ puzzles/puzzles.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def puzzle_1
 	chapter1 = File.read("input_1.txt")
 	ch1 = remove_empty_lines(chapter1)
@@ -27,5 +29,33 @@ def add_line_numbers(chapter2)
 	ch2.join("\n")
 end
 
+def puzzle_3
+	chapter3 = File.read('input_3.txt')
+	ch3 = remove_underline(chapter3)
+	print ch3
+	File.write('output_3.txt', ch3)
+end
+
+def remove_underline(chapter3)
+	ch1 = chapter3.split("_")
+	ch2 = ch1.map do |string|
+		string.to_i + 1
+	end
+	ch2.join("\n")
+end
+
+def puzzle_4
+	chapter4 = File.read('input_4.txt')
+	ch2 = reverse(chapter4)
+	ch2.add_line_numbers(chapter4)
+end
+
+def reverse(chapter4)
+	ch1 = chapter4.split("\n")
+	ch1.reverse
+end
+
 puzzle_1
 puzzle_2
+puzzle_3
+puzzle_4
