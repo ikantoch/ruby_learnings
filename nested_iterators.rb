46786eba2d42d8b6f397eccf61ab5ccc935e7635
ikantoch <ikantoch@zendesk.com>
2020-02-07 09:06:12 +0100

    playing with iterators on hash

---
+++ nested_iterators.rb
@@ -0,0 +1,20 @@
+teams = {
+	"team1" => {
+		"seat1": "Iza",
+		"seat2": "Ola",
+		"seat3": "Ada"
+	},
+
+	"team2" => {
+		"seat1": "Ula",
+		"seat2": "Ewa",
+		"seat3": "Ida"
+	}
+}
+
+teams.each do |team, employees|
+	puts team
+	employees.each do |seat, person|
+		p "#{:person} is on the #{:seat}"
+	end	
+end
