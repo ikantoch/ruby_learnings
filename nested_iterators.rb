9765345858d7ba98a802a2971b90e0f28e39f4c9
ikantoch <ikantoch@zendesk.com>
2020-02-10 08:58:01 +0100

    fixed iterator

--- nested_iterators.rb
+++ nested_iterators.rb
@@ -1,20 +1,21 @@
 teams = {
 	"team1" => {
-		"seat1": "Iza",
-		"seat2": "Ola",
-		"seat3": "Ada"
+		"seat1" => "Iza", # kluczem jest string
+		:seat2 => "Ola", # kluczem jest symbol
+		seat3: "Ada" # to samo co wyzej (kluczem jest symbol) - skrocona skladania
 	},
 
 	"team2" => {
-		"seat1": "Ula",
-		"seat2": "Ewa",
-		"seat3": "Ida"
+		"seat1" => "Ula",
+		"seat2" => "Ewa",
+		"seat3" => "Ida"
 	}
 }
 
 teams.each do |team, employees|
 	puts team
 	employees.each do |seat, person|
-		p "#{[:person]} is on the #{[:seat]}"
+		p "#{person} is on the #{seat}"
 	end	
 end
+
