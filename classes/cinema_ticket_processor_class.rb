f79bde804bda3463495a751d526c345e7085e347
ikantoch <ikantoch@zendesk.com>
2020-03-13 11:39:42 +0100

    CinemaTicket class and EmailClient class

--- classes/cinema_ticket_processor_class.rb
+++ classes/cinema_ticket_processor_class.rb
@@ -1,18 +1,43 @@
-class CinemaTicketsProcessor
-  attr_accessor :movie_name, :ticket_cost, :screening_time
+require 'sendgrid-ruby'
+require 'pry'
 
-  def initialize(movie_name, ticket_cost, screening_time)
-    @movie_name = movie_name
-    @ticket_cost = ticket_cost
-    @screening_time = screening_time
-  end
+class EmailClient
+  include SendGrid
 
-  def ticket_printer
-    puts @movie_name
-    puts @screening_time
-    puts @ticket_cost
-    puts seat_selection
+  def send_email(from, to, subject, message)
+    from = Email.new(email: from)
+    to = Email.new(email: to)
+    content = Content.new(type: 'text/plain', value: message)
+    mail = Mail.new(from, subject, to, content)
+    sg = SendGrid::API.new(api_key: ENV.fetch("SENDGRID_API_KEY"))
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
+end
+
+class CinemaTicket
+    def initialize(movie_name, ticket_cost, screening_time)
+      @movie_name = movie_name
+      @ticket_cost = ticket_cost
+      @screening_time = screening_time
+    end
+
+    def print
+      puts @movie_name
+      puts @screening_time
+      puts "#{@ticket_cost}zł"
+      puts seat_selection
+    end
+
+
+    def send_by_email(to)
+      subject = "Your ticket to #{@movie_name}"
+      from = 'no_reply@cinema.com'
+      message = 'Have fun watching the movie'
+      EmailClient.new.send_email(from, to, subject, message)
+    end
 
   private
     def seat_selection
@@ -20,5 +45,6 @@ class CinemaTicketsProcessor
     end
 end
 
-cinema_ticket = CinemaTicketsProcessor.new('Titanic', '20 zł', '18:00')
-cinema_ticket.ticket_printer
+cinema_ticket = CinemaTicket.new('Titanic', 20, '18:00')
+cinema_ticket.print
+cinema_ticket.send_by_email('test@test.com')
