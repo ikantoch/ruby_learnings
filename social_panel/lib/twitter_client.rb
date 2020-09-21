dcbe3f42cd4dfb39b3d2d63a79fc0030cf557565
ikantoch <ikantoch@zendesk.com>
2020-09-21 15:45:27 +0200

    refactored get tweet by id

--- social_panel/lib/twitter_client.rb
+++ social_panel/lib/twitter_client.rb
@@ -5,28 +5,31 @@ require 'JSON'
 require 'pry'
 
 class TwitterClient
-  URL = 'https://api.twitter.com/1.1'
-  ACCESS_TOKEN = "Bearer #{ENV['SOCIAL_PANEL_TWITTER_ACCESS_TOKEN']}"
 
-  # def initialize(access_token: ENV['SOCIAL_PANEL_TWITTER_ACCESS_TOKEN'])
-  #   @access_token = "Bearer #{access_token}"
-  # end
+  # URL = 'https://api.twitter.com/1.1'
+  # TOKEN = "Bearer #{ENV.fetch('SOCIAL_PANEL_TWITTER_ACCESS_TOKEN')}"
+
+  def connection
+      Faraday.new(
+        url: 'https://api.twitter.com/1.1',
+        headers: { 'Authorization' => "Bearer #{ENV['SOCIAL_PANEL_TWITTER_ACCESS_TOKEN']}" },
+      )
+  end
 
   def get_tweet_by_id(tweet_id)
-    response = Faraday.get("#{URL}/statuses/show.json?id=#{tweet_id}") do |request|
-      request.headers['Authorization'] = ACCESS_TOKEN
-    end
+    response = connection.get("statuses/show.json?id=#{tweet_id}")
 
     body = response.body
     JSON.parse(body)
   end
 
-  def search_tweets_by_phrase(query_phrase:, result_type: "recent", count: 1, lang: "en", locale: "us")
-    response =  Faraday.get("#{URL}/search/tweets.json?q=#{query_phrase}&#{result_type}&#{count}&#{lang}&#{locale}") do |request|
-      request.headers['Authorization'] = ACCESS_TOKEN
-    end
+  def search_tweets_by_phrase(query_phrase, result_type: "recent")
+    response =  connection.get("search/tweets.json?q=#{query_phrase}&#{result_type}")
 
     body = response.body
     JSON.parse(body)
   end
 end
+
+result = TwitterClient.new.get_tweet_by_id('1305411679468113920')
+p result
